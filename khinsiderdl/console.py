59ebe922a663f346858facfac6557cc727cda6df
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2015-07-01 23:12:47 +0800

    Parse argument

--- khinsiderdl/console.py
+++ khinsiderdl/console.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description='KHInsider Downloader'
+    )
+
+    parser.add_argument('url',
+        metavar='url',
+        type=str,
+        nargs='+',
+        help='URL to download')
+
+    parser.add_argument('--output', '-o',
+        metavar='output',
+        default='output',
+        type=str,
+        nargs=1,
+        help='Output directory')
+
+    return parser.parse_args()
 
 def main():
+    args = get_args()
+
     return 0
